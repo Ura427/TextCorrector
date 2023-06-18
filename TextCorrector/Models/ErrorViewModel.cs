aebf5bb50ad9ec777703e6f88f3ebeed0083e5b2
Yura <urakost427@gmail.com>
2023-06-18 19:39:04 +0300

    Add project files.

---
+++ TextCorrector/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TextCorrector.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
